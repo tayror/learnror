51ca20b1ae45b0cb9e7c8015cfdf92f9d0a18ef0
tayror <nguyenwallace@gmail.com>
2020-01-15 20:51:26 +0700

    new Articles Controller & Model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.htmlroot 'welcome#index'
-root 'welcome#index'
+ 
+  resources :articles
+ 
+  root 'welcome#index'
 
 end
